cee2758717e64de79bb90447500771bc7cba4b68
copy-del <dlxlrkxdmssj@gmail.com>
2025-05-02 20:15:46 +0900

    update: backend init

---
+++ backend/cleaning-guardian/src/main/java/com/example/demo/CleaningGuardianApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleaningGuardianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleaningGuardianApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
