b8199e006d7f71ece2120da116fdf6c6c88727b4
JSOCK97 <spdlqjanf@naver.com>
2025-05-20 12:29:02 +0900

    feat: pwa 및 service-worker 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,38 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'logo192.png', 'logo512.png'],
+            manifest: {
+                name: '청소의 신',
+                short_name: 'guardian',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#ffffff',
+                theme_color: '#4CAF50',
+                icons: [
+                    {
+                        src: 'logo192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'logo512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+            strategies: 'generateSW',
+            injectRegister: 'auto',
+        }),
+    ],
     resolve: {
         alias: [
             {
