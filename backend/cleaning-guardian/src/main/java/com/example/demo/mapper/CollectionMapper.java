6d3e006d92b50bd7006416cce0d9a7d298115e64
JinSeok Ock <spdlqjanf@naver.com>
2025-05-25 12:35:49 +0900

    fix: xml 수정

--- backend/cleaning-guardian/src/main/java/com/example/demo/mapper/CollectionMapper.java
+++ backend/cleaning-guardian/src/main/java/com/example/demo/mapper/CollectionMapper.java
@@ -4,13 +4,13 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import com.example.demo.vo.PollutionVO;
+import com.example.demo.vo.PollutionsVO;
 import com.example.demo.vo.UserCollectionVO;
 
 @Mapper
 public interface CollectionMapper {
-    List<PollutionVO> selectAllPollutions();
+    List<PollutionsVO> selectAllPollutions();
     List<UserCollectionVO> selectUserCollections(@Param("email") String email);
-    PollutionVO selectPollution(@Param("polIdx") Long polIdx);
+    PollutionsVO selectPollution(@Param("polIdx") Long polIdx);
     UserCollectionVO selectUserCollection(@Param("polIdx") Long polIdx);
 } 
